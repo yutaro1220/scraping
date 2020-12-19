f8b9541e54f8afec7bff6f5ea4775e999614ec39
yutaro1220 <szkytr1220@gmail.com>
2020-12-19 16:59:35 +0900

    修正

---
+++ screiping/Gyudon.swift
@@ -0,0 +1,13 @@
+//
+//  Gyudon.swift
+//  screiping
+//
+//  Created by me to you on 2020/12/19.
+//
+
+import Foundation
+
+class Gyudon: NSObject {
+    var size = ""
+    var price = ""
+}
--- screiping/GyudonPriceTableViewController.swift
+++ screiping/GyudonPriceTableViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  screiping
-//
-//  Created by me to you on 2020/12/15.
-//
 
 import UIKit
 //HTTP通信してくれるやつ
@@ -11,37 +5,35 @@ import Alamofire
 //スクレイピングしてくれるやつ
 import Kanna
 
-class nk225TableViewController: UITableViewController {
-    var beefbowl = [File]()
+class GyudonPriceTableViewController: UITableViewController {
+    var beefbowl = [Gyudon]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getGyudonPrice()
+        getGyudonPrice()
     }
     func getGyudonPrice() {
-    //スクレイピング対象のサイトを指定
+        //スクレイピング対象のサイトを指定
         AF.request("https://www.nikkei.com/markets/worldidx/chart/nk225/").responseString { response in
             switch response.result {
             case let .success(value):
                 if let doc = try? HTML(html: value, encoding: .utf8) {
                     
                     // 牛丼のサイズをXpathで指定
-                    var sizes = [String]()
+                    var sizess = [String]()
                     for link in doc.xpath("//span[@class='economic_value_now a-fs26']") {
-                        sizes.append(link.text ?? "")
+                        sizess.append(link.text ?? "")
                     }
-                    
                     //牛丼の値段をXpathで指定
                     var prices = [String]()
                     for link in doc.xpath("//span[@class='economic_balance_value a-fs18']") {
                         prices.append(link.text ?? "")
                     }
-                    
                     //牛丼のサイズ分だけループ
-                    for (index, value) in sizes.enumerated() {
-                        let gyudon = File()
-                        gyudon.size = value
-                        gyudon.price = prices[index]
-                        self.beefbowl.append(gyudon)
+                    for (index, value) in sizess.enumerated() {
+                        let gyudo = Gyudon()
+                        gyudo.size = value
+                        gyudo.price = prices[index]
+                        self.beefbowl.append(gyudo)
                     }
                     self.tableView.reloadData()
                 }
@@ -49,23 +41,28 @@ class nk225TableViewController: UITableViewController {
                 print(error)
             }
         }
-            }
-
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.beefbowl.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        let nk225 = self.beefbowl[indexPath.row]
-        cell.textLabel?.text = nk225.size
-        cell.detailTextLabel?.text = nk225.price
-
+        
+        let gyudonn = self.beefbowl[indexPath.row]
+        cell.textLabel?.text = gyudonn.size
+        cell.detailTextLabel?.text = gyudonn.price
+        
         return cell
     }
+
+    @IBAction func button(_ sender: Any) {
+    }
+    
+
 }
